22b39d6246cc02225c758a8f46ef47b0ac8d6528
Armando Rojas <34174895+rojasarmando@users.noreply.github.com>
2025-07-28 21:27:10 -0400

    EDIT: Remove space

--- filtrado.js
+++ filtrado.js
@@ -1,9 +1,8 @@
-angular.module('Lsg', []).controller('filtradoController', filtrado )
+angular.module('Lsg', []).controller('filtradoController', filtrado)
 
+function filtrado($scope, $http) {
 
-function filtrado ($scope , $http ){
-
-	v = this 
+	v = this
 
 	v.titulo = "Registrar Persona"
 	v.titulo2 = "Controles para el Filtrado"
@@ -12,105 +11,83 @@ function filtrado ($scope , $http ){
 
 
 	v.ocupacion = [
-
-		{texto : 'Programador'},
-		{texto : 'Maestro'},
-		{texto : 'Mantenido'}
+		{ texto: 'Programador' },
+		{ texto: 'Maestro' },
+		{ texto: 'Consultor' }
 	];
 
-
 	v.personas = [
-		
+
 		{
-			cedula : '26059573',
-			nombre : 'Armando',
+			cedula: '2600000001',
+			nombre: 'Armando',
 			apellido: 'Rojas',
-			ocupacion_id : 0
+			ocupacion_id: 0
 		},
 		{
-			cedula : '12964334',
-			nombre : 'Norkis',
+			cedula: '12000000002',
+			nombre: 'Norkis',
 			apellido: 'Querales',
-			ocupacion_id : 1
+			ocupacion_id: 1
 		}
 
 	];
 
+	v.asignarNuevaOcupacion = function () {
 
-	v.asignarNuevaOcupacion = function(){
-
-		v.ocupacion.push({ texto : v.agregarOcupacion   });
+		v.ocupacion.push({ texto: v.agregarOcupacion });
 		v.agregarOcupacion = ''
-	}	
+	}
 
+	v.asignarNuevaPersona = function () {
 
-	v.asignarNuevaPersona = function(){
+		v.personas.push({
 
-		v.personas.push({ 
-
-			nombre : v.nombre,
-			apellido : v.apellido,
-			cedula : v.cedula,
-			ocupacion_id : v.ocupacion_id 
-		 })
+			nombre: v.nombre,
+			apellido: v.apellido,
+			cedula: v.cedula,
+			ocupacion_id: v.ocupacion_id
+		})
 
 		v.nombre = ''
 		v.apellido = ''
 		v.cedula = ''
-		
+
 	}
+	// validacion 
+
+	inputs = ['v.cedula', 'v.nombre', 'v.apellido']
+
+	v.control = false;
+	v.cedula = '';
+	v.nombre = '';
+	v.apellido = '';
+
+	document.getElementsByTagName('select')[0].value = '0'
+	document.getElementsByTagName('select')[1].value = '0'
+
+	$scope.$watchGroup(inputs, (nuevo, anterior) => {
+
+		v.error = [];
+		v.control = true
 
 
-	  // validacion 
+		for (var i = 0; i < 3; i++) {
 
-	 inputs = [ 'v.cedula' , 'v.nombre' , 'v.apellido']
+			validaCampo(i);
+		}
 
-	 v.control = false;
-	 v.cedula = '';
-	 v.nombre = '';
-	 v.apellido = '';
+		function validaCampo(indice) {
+			if (nuevo[indice].length < 3) {
 
-	 document.getElementsByTagName('select')[0].value= '0'
-	 document.getElementsByTagName('select')[1].value= '0'
+				v.error[indice] = 'Debe ser mayor a 3 caracteres...'
+				v.control = false
 
+			}
+			else
+				v.error[indice] = 'Campo listo'
 
-
-    $scope.$watchGroup( inputs , (nuevo,anterior)=>{
-
-    	v.error= []; 
-    	v.control = true
-   
-
-    	for (var i = 0; i < 3; i++) {
-
-    		validaCampo(i);
-    	}
-
-    	
-    	
-    		
-
-
-          function validaCampo(indice){
-          	 if (nuevo[indice].length < 3) {
-
-            	v.error[indice] = 'Debe ser mayor a 3 caracteres...'
-            	v.control = false 
-
-        	}
-            else
-            	v.error[indice]  = 'Campo listo'
-             
-          }
-
-    	
-
-          
-    })
-
-
-
-
-
+		}
+	})
 
 }
